57e68a7d8551bd847b3fe9b0599a9bbff5e06576
Winnie <yang.feng.zhang@avanade.com>
2018-03-12 15:21:21 -0700

    finished module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,16 +3,22 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'; //import this to use the map function
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json'; //creating private class property
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { } //injecting an instance of a http service into the ProductService class.
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json()); 
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response)=> <Product[]>response.json());
   }
 
 }
