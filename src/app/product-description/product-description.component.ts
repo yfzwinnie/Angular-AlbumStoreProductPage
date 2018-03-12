af7bba6c219c4eece557725cc6b5d63115232f44
Winnie <yang.feng.zhang@avanade.com>
2018-03-12 13:24:59 -0700

    halfway through module 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service'; //import service in order to use it
 
 @Component({
   selector: 'app-product-description',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  albumInfo; //creating an albumInfo property within the class to be used below
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response) //kicks off the http request and stores the response in the class variable name albumInfo
   }
 
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'; //import this to use the map function
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json'; //creating private class property
+
+  constructor(private _http: Http) { } //injecting an instance of a http service into the ProductService class.
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json()); //we want the response to be json format
+  }
+
+}
