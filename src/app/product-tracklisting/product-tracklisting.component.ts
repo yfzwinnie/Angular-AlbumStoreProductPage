3452b7a5b13a639f5e9d40557dbcbff26fac9cd9
Winnie <yang.feng.zhang@avanade.com>
2018-03-12 14:26:21 -0700

    finished half of module 5

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
